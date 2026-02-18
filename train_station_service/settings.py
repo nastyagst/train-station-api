a714034929aee2dfbe55379b2cd36e13846a369e
nastya <hociy2015@gmail.com>
2026-02-18 16:57:10 +0200

    Remove .idea, add .env.sample and secure SECRET_KEY

--- train_station_service/settings.py
+++ train_station_service/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3wrt5@r@_y&!2(g@3ny69%(me_z2)jjj)b@^b7nik32%o#to9)"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-default-key")
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
